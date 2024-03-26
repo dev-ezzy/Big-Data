23519e1a650758f80f1d4e604d822e6f6769653c
dev-ezzy <bettezra07@gmail.com>
2024-03-26 10:39:37 +0300

    commit

--- functions.py
+++ functions.py
@@ -45,3 +45,27 @@ def analyze_df(df):
     # Print data types of each column
     print("Column Data Types:")
     print(df.dtypes)
+
+
+# EDA
+#writting helper function to help us make x-axis countplots in our EDA process
+def sns_xcount(column , data):
+    sns.countplot(x = column, data = data)
+    plt.title(f"{column} count in our data set")
+    plt.show();
+
+#writting helper function to help us make y-axis countplots in our EDA process
+def sns_ycount(column , data):
+    sns.countplot(y = column, data = data)
+    plt.title(f"{column} count in our data set")
+    plt.show();
+    
+#helper function for plotting multivariate countplots
+def multivariate_ycountplot(column, data):
+    sns.countplot(y = column, data = data, hue= "status_group", palette= "bright");
+    plt.legend(loc= "best")
+
+def multivariate_xcountplot(column, data):
+    sns.countplot(x = column, data = data,  hue= "status_group", palette= "bright");
+    plt.xticks(rotation = 45)
+    plt.legend(loc= "best")
