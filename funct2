0f28d67ec9a896e8cc7e47a8e5d21acb5410264e
dev-ezzy <bettezra07@gmail.com>
2024-03-26 10:55:15 +0300

    commit

---
+++ funct2
@@ -0,0 +1,23 @@
+
+# EDA
+#writting helper function to help us make x-axis countplots in our EDA process
+def sns_xcount(column , data):
+    sns.countplot(x = column, data = data)
+    plt.title(f"{column} count in our data set")
+    plt.show();
+
+#writting helper function to help us make y-axis countplots in our EDA process
+def sns_ycount(column , data):
+    sns.countplot(y = column, data = data)
+    plt.title(f"{column} count in our data set")
+    plt.show();
+    
+#helper function for plotting multivariate countplots
+def multivariate_ycountplot(column, data):
+    sns.countplot(y = column, data = data, hue= "status_group", palette= "bright");
+    plt.legend(loc= "best")
+
+def multivariate_xcountplot(column, data):
+    sns.countplot(x = column, data = data,  hue= "status_group", palette= "bright");
+    plt.xticks(rotation = 45)
+    plt.legend(loc= "best")
--- functions.py
+++ functions.py
@@ -3,7 +3,6 @@ def Libraries():
     import numpy as np
     import seaborn as sns
     import matplotlib.pyplot as plt
-    
 
 def analyze_df(df):
     """
@@ -42,26 +41,3 @@ def analyze_df(df):
     print("Column Data Types:")
     print(df.dtypes)
 
-
-# EDA
-#writting helper function to help us make x-axis countplots in our EDA process
-def sns_xcount(column , data):
-    sns.countplot(x = column, data = data)
-    plt.title(f"{column} count in our data set")
-    plt.show();
-
-#writting helper function to help us make y-axis countplots in our EDA process
-def sns_ycount(column , data):
-    sns.countplot(y = column, data = data)
-    plt.title(f"{column} count in our data set")
-    plt.show();
-    
-#helper function for plotting multivariate countplots
-def multivariate_ycountplot(column, data):
-    sns.countplot(y = column, data = data, hue= "status_group", palette= "bright");
-    plt.legend(loc= "best")
-
-def multivariate_xcountplot(column, data):
-    sns.countplot(x = column, data = data,  hue= "status_group", palette= "bright");
-    plt.xticks(rotation = 45)
-    plt.legend(loc= "best")
