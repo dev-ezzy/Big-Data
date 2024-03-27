bec9378a3f6276d3b9a651a97f2bc2baa9924cbf
dev-ezzy <bettezra07@gmail.com>
2024-03-27 11:27:11 +0300

    commit

--- funct2.py
+++ funct2.py
@@ -1,15 +1,13 @@
 # EDA
 #writting helper function to help us make x-axis countplots in our EDA process
 def sns_xcount(column , data):
-    sns.countplot(x = column, data = data)
-    sns.color_pallete("tab_10")
+    sns.countplot(x = column, data = data, pallete = "tab_10")
     plt.title(f"{column} count in our data set")
     plt.show();
 
 #writting helper function to help us make y-axis countplots in our EDA process
 def sns_ycount(column , data):
     sns.countplot(y = column, data = data)
-    sns.color_pallete("tab_10")
     plt.title(f"{column} count in our data set")
     plt.show();
     
